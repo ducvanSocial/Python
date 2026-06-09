f8352de6ffb37e548ee7fa706dca1b03b2862f69
ducvansocial <ducnguyennv20618@gmail.com>
2026-06-09 13:34:14 +0700

    Remove unused import of admin from saleapp

--- __init__.py
+++ __init__.py
@@ -5,6 +5,4 @@ app.secret_key = "1234567890aaa"
 app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:123456@localhost/dataqlspdb?charset=utf8mb4"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
-
-from saleapp import admin
 from saleapp import routes
