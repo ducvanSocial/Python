206b3286db924b41f763b21b5380b7a7a7fe9b62
ducvansocial <ducnguyennv20618@gmail.com>
2026-06-09 17:19:12 +0700

    Clean up unused product functions in utils.py
    Removed several unused product-related functions from utils.py.

--- utils.py
+++ utils.py
@@ -13,53 +13,7 @@ def load_masp():
     return Masanpham.query.all()
 
 
-def load_san_pham(maid=None, ten=None, giabatdau=None):
-    products = Danhmucsanpham.query
-    if maid:
-        products = products.filter(Danhmucsanpham.masanpham == int(maid))
-    if ten:
-        products = products.filter(Danhmucsanpham.name == ten)
-    if giabatdau:
-        products = products.filter(Danhmucsanpham.giatien >= (giabatdau))
-    return products
-
-
-
-def trasp(product_id):
-    products = Danhmucsanpham.query.all()
-    for p in products:
-        if p.id == product_id:
-            return p
-
-
-
-def read_product_by(lay_id):
-    products = Danhmucsanpham.query.all()
-    for x in products:
-        if x.id == lay_id:
-            return x
-        
-
-def thanhtoan(id):
-    sanpham = Danhmucsanpham.query.all()
-    for s in sanpham:
-        if s.id == id:
-            return s
-
-
 def dangnhap(ten, matkhau):
     user = User.query.filter_by(ten=ten).first()
     if user and user.matkhau == matkhau:
         return user
-
-
-def add_user(name, username, password):
-    user = User(name=name, ten=username, matkhau=password)
-    db.session.add(user)
-    db.session.commit()
-
-
-def themsanpham(name, ghichu ,giatien, anh,masanpham):
-    p = Danhmucsanpham(name=name, ghichu=ghichu, giatien=giatien, anh=anh, masanpham=masanpham)
-    db.session.add(p)
-    db.session.commit()
